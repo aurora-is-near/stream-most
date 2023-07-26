7f0e15956781a49ff40805829c9605fa056bd896
Dmitry Strokov <dmitry.strokov@aurora.dev>
2023-07-26 11:59:57 +0400

    domain/blocks: remove sequence from AbstractBlock (unused)

--- domain/blocks/abstract.go
+++ domain/blocks/abstract.go
@@ -4,10 +4,4 @@ type AbstractBlock struct {
 	Hash     string
 	PrevHash string
 	Height   uint64
-	Sequence uint64
-}
-
-func (b *AbstractBlock) WithSequence(sequence uint64) *AbstractBlock {
-	b.Sequence = sequence
-	return b
 }
