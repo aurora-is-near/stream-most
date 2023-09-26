ae9d4fa2e0eb00bd4aeb6e552c07dea2fcca8c3d
Dmitry Strokov <dmitry.strokov@aurora.dev>
2023-09-26 17:19:23 +0600

    add early bridge files

--- service2/bridge/bridge.go
+++ service2/bridge/bridge.go
@@ -1,1 +1,12 @@
 package bridge
+
+import (
+	"github.com/aurora-is-near/stream-most/service2/blockio"
+	"github.com/aurora-is-near/stream-most/service2/verifier"
+)
+
+type Bridge struct {
+	verifier verifier.Verifier
+	input    blockio.Input
+	output   blockio.Output
+}
--- service2/bridge/config.go
+++ service2/bridge/config.go
@@ -1,22 +1,4 @@
 package bridge
 
-import (
-	"time"
-
-	"github.com/aurora-is-near/stream-most/service2/blockwriter"
-	"github.com/aurora-is-near/stream-most/service2/drivers/aligner"
-	"github.com/aurora-is-near/stream-most/stream"
-	"github.com/aurora-is-near/stream-most/stream/reader"
-)
-
 type Config struct {
-	Format      string
-	HeadersOnly bool
-
-	Input            *stream.Options
-	Reader           *reader.Options
-	Output           *stream.Options
-	TipCheckInterval time.Duration
-	Aligner          *aligner.Config
-	Writer           *blockwriter.Config
 }
