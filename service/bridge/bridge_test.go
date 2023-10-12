23fe903acea8d02877c179e1fef487278ee46b07
Dmitry Strokov <dmitry.strokov@aurora.dev>
2023-10-13 04:31:20 +0600

    implement bridge logic

---
+++ service/bridge/bridge_test.go
@@ -0,0 +1,14 @@
+package bridge
+
+/*
+	TODO tests:
+		1. Basic bridging
+		2. Tolerance violation
+		3. Reseeking
+		4. Unavailable input/output
+		5. Reordering
+		6. Anchoring
+		7. End/last block
+		8. Start/end input seq
+		9. Concurrent writer
+*/
